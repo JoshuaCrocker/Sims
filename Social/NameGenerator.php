14b7567b1dbe9304bf96e46c41ca6a8994ed1415
Joshua Crocker <joshua@auratemple.net>
2016-12-11 18:32:01 +0000

    Implemented seeding with NameGenerator

--- Social/NameGenerator.php
+++ Social/NameGenerator.php
@@ -2,11 +2,14 @@
 class NameGenerator {
     const API_URL = 'https://randomuser.me/api/?nat=gb';
 
+    private $seed;
+
     private $gender;
     private $firstName;
     private $lastName;
 
-    public function __construct() {
+    public function __construct($seed = null) {
+        $this->seed = is_null($seed) ? sha1(time()) : $seed;
         $this->regenerate();
     }
 
@@ -27,11 +30,19 @@ class NameGenerator {
     }
 
     public function regenerate() {
-        $json = file_get_contents(NameGenerator::API_URL);
+        $json = file_get_contents(NameGenerator::API_URL.'?seed' . $this->seed);
         $json = json_decode($json);
 
         $this->gender = $json->results[0]->gender == 'male' ? Person::GENDER_MALE : Person::GENDER_FEMALE;
         $this->firstName = ucfirst($json->results[0]->name->first);
         $this->lastName = ucfirst($json->results[0]->name->last);
+    }
+
+    public function setSeed($seed) {
+        $this->seed = $seed;
+    }
+
+    public function getSeed($seed) {
+        return $this->seed;
     }
 }
